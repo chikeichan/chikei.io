6f17597bd99358c28b50387b11524ebadb6b3c1a
chikeichan <chikeichan@gmail.com>
2015-09-23 10:28:15 -0700

    Added metadata to blog reader

--- client/src/modules/blogReader/component/style/blogReaderStyle.js
+++ client/src/modules/blogReader/component/style/blogReaderStyle.js
@@ -7,15 +7,13 @@ Trio.Module.export('blogReaderStyle', function() {
             'width': '100%'
         },
         '.nav': {
-            // 'background-color': '#1F1F1F',
             'background-color': Trio.Stylizer.toRGBa(Trio.Stylizer.getVariable('theme-color'), 0.4),
-            'border-right': '2px solid black',
+            'border-right': '2px solid ' + Trio.Stylizer.getVariable('base-color'),
             'width': '200px',
             'position': 'relative',
             'flex': '0 0 auto'
         },
         '.blog-content': {
-            // 'background-color': '#3A3A3A',
             'width': '100%',
             'display': 'flex',
             'flex-flow': 'column nowrap',
@@ -24,20 +22,28 @@ Trio.Module.export('blogReaderStyle', function() {
         },
         '.nav-folder': {
             'display': 'flex',
-            'padding': '12px',
-            'margin': '2px 0',
+            'margin': '1px 0',
             'font-weight': '100',
-            'color': Trio.Stylizer.getVariable('layout-color'),
-            'text-transform': 'uppercase',
-            'cursor': 'pointer',
             'background-color': 'rgba(0, 0, 0, 0.1)',
-            '-webkit-user-select': 'none'
+            '-webkit-user-select': 'none',
+            'cursor': 'pointer',
+            'flex-flow': 'column nowrap'
         },
         '.nav-folder:hover': {
             'background-color': 'rgba(0, 0, 0, 0.05)'
         },
         '.nav-folder:active': {
             'background-color': 'rgba(0, 0, 0, 0.15)'
+        },
+        '.nav-folder-name': {
+            'color': Trio.Stylizer.getVariable('layout-color'),
+            'text-transform': 'uppercase',
+            'padding': '12px'
+        },
+        '.nav-folder-content': {
+            'display': 'flex',
+            'flex-flow': 'column nowrap',
+            'overflow': 'hidden'
         },
         '.nav-item': {
             'padding': '4px 4px 4px 16px',
@@ -50,7 +56,8 @@ Trio.Module.export('blogReaderStyle', function() {
             '-webkit-user-select': 'none',
             'white-space': 'nowrap',
             'text-overflow': 'ellipsis',
-            'overflow': 'hidden'
+            'overflow': 'hidden',
+            'flex': '1 0 auto'
         },
         '.nav-item:hover': {
             'background-color': 'rgba(255, 255, 255, 0.15)'
@@ -71,7 +78,17 @@ Trio.Module.export('blogReaderStyle', function() {
             'font-weight': '300',
             'color': Trio.Stylizer.getVariable('layout-color'),
             'padding': '12px 24px',
+        },
+        '.blog-meta': {
+            'display': 'flex',
+            'flex-flow': 'row nowrap',
             'border-bottom': '1px solid ' + Trio.Stylizer.toRGBa(Trio.Stylizer.getVariable('layout-color'), 0.2)
+        },
+        '.blog-date': {
+            'font-size': '0.7em',
+            'font-weight': '300',
+            'color': Trio.Stylizer.getVariable('layout-color'),
+            'padding': '12px 24px',
         },
         '.blog-body': {
             'display': 'flex',
--- client/src/modules/blogReader/service/blogReaderService.js
+++ client/src/modules/blogReader/service/blogReaderService.js
@@ -1,12 +1,22 @@
 Trio.Module.export('blogReaderService', function() {
     var folderTmpl = Trio.Renderer.createTemplate();
-        folderTmpl.create('div.nav-folder').text(function(d) {return d.title}).appendLast();
+        folderTmpl.create('div.nav-folder')
+                .create('div.nav-folder-name').text(function(d) {return d.title}).append()
+                .create('div.nav-folder-content').append()
+            .appendLast();
     var itemTmpl = Trio.Renderer.createTemplate();
         itemTmpl.create('div.nav-item').text(function(d) {return d.title}).appendLast();
 
     var blogTmpl = Trio.Renderer.createTemplate();
         blogTmpl.create('div.blog-header').text(function(d) {return d.title}).append()
-            .create('div.blog-author').text(function(d) {return d.author.name}).append()
+            .create('div.blog-meta')
+                .create('div.blog-author').text(function(d) {return d.author.name}).append()
+                .create('div.blog-date').text(function(d) {return new Date(+d.timestamp).toString().slice(4, 15)}).append()
+                .create('div.fb-share-button')
+                    .attr('data-href', "https://developers.facebook.com/docs/plugins/")
+                    .attr('data-layout', 'button')
+                .append()
+            .append()
             .create('div.blog-body').text(function(d) {return d.content}).appendLast()
 
     var BlogReaderService = Trio.Service.extend({
@@ -19,26 +29,29 @@ Trio.Module.export('blogReaderService', function() {
 
         initStructure: function(root, list) {
             list.forEach(function(item) {
-                var el;
+                var itemEl;
                 if (!item.threadId) {
-                    el = createFolder(item);
+                    itemEl = createFolder.call(this, item);
                     if (item.children) {
-                        this.initStructure(el, item.children);
+                        this.initStructure(itemEl.querySelector('.nav-folder-content'), item.children);
                     }
                 } else {
-                    el = createItem.call(this, item);    
+                    itemEl = createItem.call(this, item);    
                 }
-                root.appendChild(el);
+                root.appendChild(itemEl);
             }.bind(this))
 
             function createFolder(d) {
                 var folder = folderTmpl.render(d);
+                folder.querySelector('.nav-folder-name')
+                    .addEventListener('click', this.toggleFolder.bind(this, folder.querySelector('.nav-folder-content')));
                 return folder;
             }
 
             function createItem(d) {
                 var item = itemTmpl.render(d);
-                item.querySelector('.nav-item').addEventListener('click', this.openBlog.bind(this, d.threadId));
+                item.querySelector('.nav-item')
+                    .addEventListener('click', this.openBlog.bind(this, d.threadId));
                 return item;
             }
         },
@@ -47,6 +60,11 @@ Trio.Module.export('blogReaderService', function() {
             var blog = this.factory.fetchBlog(id);
             var el = blogTmpl.render(blog);
             this.component.renderBlog(el);
+        },
+
+        toggleFolder: function(el) {
+            el.style.height = el.isCollapsed ? '' : '0px';
+            el.isCollapsed = !el.isCollapsed;
         }
 
     });
