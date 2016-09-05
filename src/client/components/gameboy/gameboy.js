11e6da921f29fc11bd095c408e99b2dc7b2597eb
Jacky Chan <jchan@uber.com>
2016-09-05 14:02:44 -0700

    Add tetris transitions and gamefield

---
+++ src/client/components/gameboy/gameboy.js
@@ -0,0 +1,103 @@
+import React, {Component, PropTypes} from 'react';
+import classnames from 'classnames';
+
+class Gameboy extends Component {
+  static propTypes = {
+    isOn: PropTypes.bool
+  };
+
+  static defaultProps = {
+    isOn: false
+  };
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.isOn !== this.props.isOn;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.isOn) {
+      const startup = new Audio('./sounds/gameboy-startup.wav');
+      startup.play();
+    }
+  }
+
+  render() {
+    const {children, isOn} = this.props;
+    const statusClass = classnames(
+      'gameboy-screen',
+      {
+        'gameboy-screen-on': isOn
+      }
+    );
+    return (
+      <div className="gameboy-wrapper">
+        <div className="gameboy-top">
+          <div className="gameboy-top-left"/>
+          <div className="gameboy-top-logo">Minkendo</div>
+          <div className="gameboy-top-center"/>
+          <div className="gameboy-top-right"/>
+        </div>
+        <div className="gameboy-screen-wrapper">
+          <div className="gameboy-screen-header"/>
+          <div className={statusClass}>
+            <div
+              className="gameboy-content-wrapper"
+              style={{
+                opacity: isOn ? 1 : 0,
+                height: isOn ? 170 : 0
+              }}>
+              {children}
+            </div>
+          </div>
+        </div>
+        <div className="gameboy-branding">
+          <div className="gameboy-company">Minkendo</div>
+          <div className="gameboy-console">Game Kid</div>
+          <div className="gameboy-tm">TM</div>
+        </div>
+        <div className="gameboy-control">
+          <div className="gameboy-dpad">
+            <div className="gameboy-dpad-row gameboy-dpad-row--one">
+              <div className="gameboy-dpad-up"/>
+            </div>
+            <div className="gameboy-dpad-row gameboy-dpad-row--two">
+              <div className="gameboy-dpad-left"/>
+              <div className="gameboy-dpad-center"/>
+              <div className="gameboy-dpad-right"/>
+            </div>
+            <div className="gameboy-dpad-row gameboy-dpad-row--three">
+              <div className="gameboy-dpad-down"/>
+            </div>
+          </div>
+          <div className="gameboy-buttons">
+            <div className="gameboy-button-a"/>
+            <div className="gameboy-button-divider"/>
+            <div className="gameboy-button-b"/>
+          </div>
+        </div>
+        <div className="gameboy-alt-buttons">
+          <div className="gameboy-alt-button">
+            <div className="gameboy-alt-select"/>
+            SELECT
+          </div>
+          <div className="gameboy-alt-button">
+            <div className="gameboy-alt-start"/>
+            START
+          </div>
+        </div>
+        <div className="gameboy-speakers">
+          <div className="gameboy-speaker-wrapper">
+            <div className="gameboy-speaker"/>
+            <div className="gameboy-speaker"/>
+            <div className="gameboy-speaker"/>
+            <div className="gameboy-speaker"/>
+            <div className="gameboy-speaker"/>
+            <div className="gameboy-speaker"/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Gameboy;
--- src/client/components/tetris/tetris.js
+++ src/client/components/tetris/tetris.js
@@ -1,79 +1,42 @@
 import React, {Component, PropTypes} from 'react';
 import Window from '../../containers/window-container/window-container';
 import ActionBar from './tetris-action-bar';
+import Gameboy from '../gameboy/gameboy';
 
-class Folder extends Component {
+class Tetris extends Component {
   static propTypes = {
   };
 
   static defaultProps = {
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: false
+    };
+  }
+
+  componentWillMount() {
+    setTimeout(() => this.setState({isOn: true}), 0);
+  }
+
   render() {
     const {actions} = this.props;
     return (
       <Window {...this.props}>
         <ActionBar actions={actions} />
-        <div className="gameboy-wrapper">
-          <div className="gameboy-top">
-            <div className="gameboy-top-left"/>
-            <div className="gameboy-top-logo">Minkendo</div>
-            <div className="gameboy-top-center"/>
-            <div className="gameboy-top-right"/>
-          </div>
-          <div className="gameboy-screen-wrapper">
-            <div className="gameboy-screen-header"/>
-            <div className="gameboy-screen"/>
-          </div>
-          <div className="gameboy-branding">
-            <div className="gameboy-company">Minkendo</div>
-            <div className="gameboy-console">Game Kid</div>
-            <div className="gameboy-tm">TM</div>
-          </div>
-          <div className="gameboy-control">
-            <div className="gameboy-dpad">
-              <div className="gameboy-dpad-row gameboy-dpad-row--one">
-                <div className="gameboy-dpad-up"/>
-              </div>
-              <div className="gameboy-dpad-row gameboy-dpad-row--two">
-                <div className="gameboy-dpad-left"/>
-                <div className="gameboy-dpad-center"/>
-                <div className="gameboy-dpad-right"/>
-              </div>
-              <div className="gameboy-dpad-row gameboy-dpad-row--three">
-                <div className="gameboy-dpad-down"/>
-              </div>
+        <Gameboy isOn={this.state.isOn}>
+          <div className="tetris-wrapper">
+            <div className="tetris-game-background">
+              <div className="tetris-game-field"/>
             </div>
-            <div className="gameboy-buttons">
-              <div className="gameboy-button-a"/>
-              <div className="gameboy-button-divider"/>
-              <div className="gameboy-button-b"/>
-            </div>
+            <div className="tetris-game-info" />
           </div>
-          <div className="gameboy-alt-buttons">
-            <div className="gameboy-alt-button">
-              <div className="gameboy-alt-select"/>
-              SELECT
-            </div>
-            <div className="gameboy-alt-button">
-              <div className="gameboy-alt-start"/>
-              START
-            </div>
-          </div>
-          <div className="gameboy-speakers">
-            <div className="gameboy-speaker-wrapper">
-              <div className="gameboy-speaker"/>
-              <div className="gameboy-speaker"/>
-              <div className="gameboy-speaker"/>
-              <div className="gameboy-speaker"/>
-              <div className="gameboy-speaker"/>
-              <div className="gameboy-speaker"/>
-            </div>
-          </div>
-        </div>
+        </Gameboy>
       </Window>
     );
   }
 }
 
-export default Folder;
+export default Tetris;
